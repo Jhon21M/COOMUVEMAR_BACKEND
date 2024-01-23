4d037a921da0bcf05c11133f862bdee3c28fb7ea
Melendez jhon <osmanmelendez208@gmail.com>
2024-01-23 10:56:07 -0600

    Implementacio de Interface

---
+++ src/finca/interfaces/finca.interface.ts
@@ -0,0 +1,8 @@
+export interface FincaInterface {
+  nombre: string;
+  comunidad: string;
+  areaCacaoProduccion: string;
+  areaCacaoDesarrollo: string;
+  produccionUltimoSiclo: string;
+  IDProductor: number;
+}
