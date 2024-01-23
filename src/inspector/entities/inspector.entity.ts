d82dea2377c0be06eb3796e752a0da0765fa2083
Melendez jhon <osmanmelendez208@gmail.com>
2024-01-23 14:50:55 -0600

    Interface para la entity

--- src/inspector/entities/inspector.entity.ts
+++ src/inspector/entities/inspector.entity.ts
@@ -1,1 +1,29 @@
-export class Inspector {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { number } from 'joi';
+import { ProductorInterface } from 'src/productor/interfaces';
+
+export class EntityInspector implements InspectorInterface {
+  @ApiProperty()
+  nombre: string;
+
+  @ApiProperty()
+  apellido: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  numeroCedula: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  numeroTelefono: string | null;
+
+  @ApiProperty({ required: false, nullable: true })
+  fechaIngresoPrograma: Date | null;
+
+  @Transform(({ value }) => value.toNumber()) // ejemplo del uso de tansform, para mostrar datos hacia el usuario
+  @ApiProperty({ type: number })
+  estado: number;
+
+  constructor(partial: Partial<EntityProductor>) {
+    Object.assign(this, partial);
+  }
+}
