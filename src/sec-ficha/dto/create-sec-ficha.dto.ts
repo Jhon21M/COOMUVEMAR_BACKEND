b32a2ad0df45ca032095e8068a996c2c80197fbf
Melendez jhon <osmanmelendez208@gmail.com>
2024-01-24 10:05:17 -0600

    Dto para capa de controlador

--- src/sec-ficha/dto/create-sec-ficha.dto.ts
+++ src/sec-ficha/dto/create-sec-ficha.dto.ts
@@ -1,1 +1,16 @@
-export class CreateSecFichaDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateSeccionFichaDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 40)
+  @ApiProperty({ required: true })
+  nombre: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 40)
+  @ApiProperty({ required: true })
+  descripcion: string;
+}
