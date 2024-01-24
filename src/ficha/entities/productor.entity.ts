084dc3584ac665ae9f2411e0405f49d2be33bb45
Melendez jhon <osmanmelendez208@gmail.com>
2024-01-23 21:16:03 -0600

    Creacion de Entity para la capa de servicio

---
+++ src/ficha/entities/productor.entity.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { FichaInterface } from 'src/ficha/interfaces';
+
+export class EntityFicha implements FichaInterface {
+  @ApiProperty({ required: false, nullable: true })
+  fecha: Date;
+
+  @ApiProperty({ required: true, nullable: false })
+  localizacion: string;
+
+  @ApiProperty({ required: true, nullable: false })
+  IDInspector: number;
+
+  constructor(partial: Partial<EntityFicha>) {
+    Object.assign(this, partial);
+  }
+}
