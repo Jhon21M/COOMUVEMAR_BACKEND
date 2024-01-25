685dc226e5ca9cac029ba70cc3c4a15545cdca2a
Melendez jhon <osmanmelendez208@gmail.com>
2024-01-24 22:23:54 -0600

    Creacion de Dto para capa controlador

--- src/info-dato/dto/create-info-dato.dto.ts
+++ src/info-dato/dto/create-info-dato.dto.ts
@@ -1,1 +1,19 @@
-export class CreateInfoDatoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateInfoDatoDto {
+  @IsNotEmpty()
+  @ApiProperty()
+  @IsString()
+  informacion: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ nullable: true })
+  descripcion: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty()
+  IDDato: number;
+}
