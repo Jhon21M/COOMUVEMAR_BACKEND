bd80117931c6ffc97d3f53e4291016c1d3f2f58c
Melendez jhon <osmanmelendez208@gmail.com>
2024-01-25 11:28:58 -0600

    Creacion de Dto

--- src/desicion/dto/create-desicion.dto.ts
+++ src/desicion/dto/create-desicion.dto.ts
@@ -1,1 +1,19 @@
-export class CreateDesicionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateDesicionDto {
+  @IsNotEmpty()
+  @ApiProperty()
+  @IsString()
+  declaracion: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ nullable: true })
+  desicion: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty({ required: true })
+  IDFicha: number;
+}
