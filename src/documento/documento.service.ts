bd7ad077f5b4cc5e1447a26760873803308a766b
Melendez jhon <osmanmelendez208@gmail.com>
2024-01-25 10:48:46 -0600

    Creacion de logica para capa servicio con entities

--- src/documento/documento.service.ts
+++ src/documento/documento.service.ts
@@ -1,26 +1,60 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDocumentoDto } from './dto/create-documento.dto';
-import { UpdateDocumentoDto } from './dto/update-documento.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EntityDocumento, EntityUpdateDocumento } from './entities';
 
 @Injectable()
 export class DocumentoService {
-  create(createDocumentoDto: CreateDocumentoDto) {
-    return 'This action adds a new documento';
+  constructor(private prisma: PrismaService) {}
+
+  async create(dato: EntityDocumento): Promise<EntityDocumento> {
+    try {
+      const newdocumento = await this.prisma.documento.create({
+        data: {
+          ...dato,
+        },
+      });
+
+      return newdocumento;
+    } catch (error) {
+      console.error('ah! algo salio mal:', error);
+      throw new Error('ah! algo salio mal!');
+    }
   }
 
-  findAll() {
-    return `This action returns all documento`;
+  async findAll() {
+    return await this.prisma.documento.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} documento`;
+    return this.prisma.documento.findUnique({
+      where: {
+        id: typeof id === 'number' ? id : Number.parseInt(id),
+      },
+    });
   }
 
-  update(id: number, updateDocumentoDto: UpdateDocumentoDto) {
-    return `This action updates a #${id} documento`;
+  async update(
+    id: number,
+    documento: EntityUpdateDocumento,
+  ): Promise<EntityUpdateDocumento> {
+    return await this.prisma.documento.update({
+      where: {
+        id: typeof id === 'number' ? id : Number.parseInt(id),
+      },
+      data: {
+        ...documento,
+      },
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} documento`;
+    return this.prisma.documento.delete({
+      where: {
+        id: typeof id === 'number' ? id : Number.parseInt(id),
+      },
+      select: {
+        id: true,
+      },
+    });
   }
 }
