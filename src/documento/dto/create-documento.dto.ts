35cd4f27b5d2b34f6cee68151d2a0183ee530846
Melendez jhon <osmanmelendez208@gmail.com>
2024-01-25 10:26:47 -0600

    Dto's para la capa controlador

--- src/documento/dto/create-documento.dto.ts
+++ src/documento/dto/create-documento.dto.ts
@@ -1,1 +1,19 @@
-export class CreateDocumentoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, IsNumber } from 'class-validator';
+
+export class CreateDocumentoDto {
+  @IsNotEmpty()
+  @ApiProperty()
+  @IsString()
+  declaracion: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ nullable: true })
+  DOCDictamenFinal: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty({ required: true })
+  IDFicha: number;
+}
